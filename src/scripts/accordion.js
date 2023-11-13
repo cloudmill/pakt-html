175f85787dc57b9e0bafab0753f088f59f2f08da
Nikita <nikita.ableev@mail.ru>
2023-11-13 15:39:22 +0300

    update conditions

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -2,7 +2,7 @@ function accordionDropdown(e, root) {
   const accordion = $(root)
   const target = $(e.target)
 
-  if (target.closest('[data-accordion-button]').length) {
+  if (target.closest('[data-accordion-button]').length && !target.closest('.checkbox__label').length) {
     target.closest(accordion).toggleClass('active');
     target.closest(accordion).find('[data-accordion-dropdown]').eq(0).slideToggle()
   }
